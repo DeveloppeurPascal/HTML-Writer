ebf6c3599ea1084030ea635060a98f9d3d99677e
Patrick PREMARTIN <patrick.premartin@developpeur-pascal.fr>
2024-02-08 11:31:27 +0100

    added the new data module in the project source (#7)

--- src/HTMLWriter.dpr
+++ src/HTMLWriter.dpr
@@ -6,12 +6,14 @@ uses
   fMain in 'fMain.pas' {Form1},
   Olf.FMX.AboutDialog in '..\lib-externes\AboutDialog-Delphi-Component\sources\Olf.FMX.AboutDialog.pas',
   Olf.FMX.AboutDialogForm in '..\lib-externes\AboutDialog-Delphi-Component\sources\Olf.FMX.AboutDialogForm.pas' {OlfAboutDialogForm},
-  u_urlOpen in '..\lib-externes\librairies\u_urlOpen.pas';
+  u_urlOpen in '..\lib-externes\librairies\u_urlOpen.pas',
+  uDMLogo in 'uDMLogo.pas' {DMLogo: TDataModule};
 
 {$R *.res}
 
 begin
   Application.Initialize;
   Application.CreateForm(TForm1, Form1);
+  Application.CreateForm(TDMLogo, DMLogo);
   Application.Run;
 end.
