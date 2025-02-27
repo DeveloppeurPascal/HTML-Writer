d7fc844ad45100e78d4739b6309d76d370a76a81
Patrick PREMARTIN <patrick.premartin@developpeur-pascal.fr>
2025-02-27 21:15:56 +0100

    added the about box description

---
+++ src/uTxtAboutDescription.pas
@@ -0,0 +1,137 @@
+﻿/// <summary>
+/// ***************************************************************************
+///
+/// HTML Writer
+///
+/// Copyright 2024-2025 Patrick PREMARTIN under AGPL 3.0 license.
+///
+/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+/// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+/// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+/// THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+/// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+/// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+/// DEALINGS IN THE SOFTWARE.
+///
+/// ***************************************************************************
+///
+/// A very simple HTML source and WYSIWYG editor for webmasters and content
+/// creators.
+///
+/// ***************************************************************************
+///
+/// Author(s) :
+/// Patrick PREMARTIN
+///
+/// Site :
+/// https://htmlwriter.olfsoftware.fr/
+///
+/// Project site :
+/// https://github.com/DeveloppeurPascal/HTML-Writer
+///
+/// ***************************************************************************
+/// File last update : 2025-02-27T20:24:26.000+01:00
+/// Signature : d79d09234e7cad92a95e2a2085217d85c7d0032f
+/// ***************************************************************************
+/// </summary>
+
+unit uTxtAboutDescription;
+
+interface
+
+function GetTxtAboutDescription(const Language: string;
+  const Recursif: boolean = false): string;
+
+implementation
+
+// For the languages codes, please use 2 letters ISO codes
+// https://en.wikipedia.org/wiki/List_of_ISO_3166_country_codes
+
+uses
+  System.SysUtils,
+  uConsts;
+
+const
+  CTxtEN = '''
+HTML Writer is a simple HTML code and WYSIWYG editor.
+
+*****************
+* Credits
+
+This application was developed by Patrick Prémartin in Delphi.
+
+The WYSIWYG editor uses Alexander Sviridenkov's Delphi HTML Components library.
+
+*****************
+* Publisher info
+
+This software is published by OLF SOFTWARE, a company registered in Paris (France) under the reference 439521725.
+
+****************
+* Personal data
+
+This program is autonomous in its current version. It does not depend on the Internet and communicates nothing to the outside world.
+
+We have no knowledge of what you do with it.
+
+No information about you is transmitted to us or to any third party.
+
+We use no cookies, no tracking, no stats on your use of the application.
+
+***************
+* User support
+
+If you have any questions or require additional functionality, please leave us a message on the application's website or on its code repository.
+
+''';
+   CTxtFR = '''
+HTML Writer est un simple éditeur de code HTML et WYSYWYG.
+
+*****************
+* Remerciements
+
+Cette application a été développée par Patrick Prémartin en Delphi.
+
+L'éditeur avec mise en forme utilise la librairie Delphi HTML Components de Alexander Sviridenkov.
+
+*****************
+* Info éditeur
+
+Ce logiciel est éditée par OLF SOFTWARE, société enregistrée à Paris (France) sous la référence 439521725.
+
+****************
+* Données personnelles
+
+Ce programme est autonome dans sa version actuelle. Il ne dépend pas d'Internet et ne communique rien au monde extérieur.
+
+Nous n'avons aucune connaissance de ce que vous faites avec lui.
+
+Aucune information vous concernant n'est transmise à nous ou à des tiers.
+
+Nous n'utilisons pas de cookies, pas de tracking, pas de statistiques sur votre utilisation de l'application.
+
+***************
+* Assistance aux utilisateurs
+
+Si vous avez des questions ou si vous avez besoin de fonctionnalités supplémentaires, veuillez nous laisser un message sur le site web de l'application ou sur son dépôt de code.
+
+''';
+
+function GetTxtAboutDescription(const Language: string;
+  const Recursif: boolean): string;
+var
+  lng: string;
+begin
+  lng := Language.tolower;
+  if (lng = 'en') then
+    result := CTxtEN
+  else if (lng = 'fr') then // France
+    result := CTxtFR
+  else if not Recursif then
+    result := GetTxtAboutDescription(CDefaultLanguage, true)
+  else
+    raise Exception.Create('Unknow description for language "' +
+      Language + '".');
+end;
+
+end.
