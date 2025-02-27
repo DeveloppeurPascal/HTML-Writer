9a9b5ba6bd74deebcedd0ca0533f07ac29f22aeb
Patrick PREMARTIN <patrick.premartin@developpeur-pascal.fr>
2025-02-27 21:15:42 +0100

    added the logo container for the about box

---
+++ src/uDMAboutBoxLogoStorrage.pas
@@ -0,0 +1,60 @@
+﻿/// <summary>
+/// ***************************************************************************
+///
+/// HTML Writer
+///
+/// Copyright 2024-2025 Patrick PREMARTIN under AGPL 3.0 license.
+///
+/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+/// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+/// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+/// THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+/// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+/// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+/// DEALINGS IN THE SOFTWARE.
+///
+/// ***************************************************************************
+///
+/// A very simple HTML source and WYSIWYG editor for webmasters and content
+/// creators.
+///
+/// ***************************************************************************
+///
+/// Author(s) :
+/// Patrick PREMARTIN
+///
+/// Site :
+/// https://htmlwriter.olfsoftware.fr/
+///
+/// Project site :
+/// https://github.com/DeveloppeurPascal/HTML-Writer
+///
+/// ***************************************************************************
+/// File last update : 2025-02-27T19:50:52.000+01:00
+/// Signature : 6e33fee54536047f86e1ddce399e4de1e6014b31
+/// ***************************************************************************
+/// </summary>
+
+unit uDMAboutBoxLogoStorrage;
+
+interface
+
+uses
+  System.SysUtils,
+  System.Classes,
+  System.ImageList,
+  FMX.ImgList;
+
+type
+  TdmAboutBoxLogo = class(TDataModule)
+    imgLogo: TImageList;
+  private
+  public
+  end;
+
+implementation
+
+{%CLASSGROUP 'FMX.Controls.TControl'}
+{$R *.dfm}
+
+end.
